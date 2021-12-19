239a7a3a0ec03373474578c3dc11c43515985ba6
martinasl <martina.slavova@abv.bg>
2021-12-19 22:38:22 +0200

    Initial commit

---
+++ src/main/java/com/aacademy/aacademyfinalproject/AacademyFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aacademy.aacademyfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AacademyFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AacademyFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aacademy/aacademyfinalproject/AacademyFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aacademy.aacademyfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AacademyFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
